f8f263effab79509f5029bf32166884117a3b876
May <may@theghostsip.eu>
2025-08-07 17:43:20 +0200

    Update proxmoxve_community.sh

--- proxmoxve_community.sh
+++ proxmoxve_community.sh
@@ -8,5 +8,5 @@ echo "deb http://download.proxmox.com/debian/pve $(grep "VERSION=" /etc/os-relea
 echo "deb http://download.proxmox.com/debian/ceph-quincy $(grep "VERSION=" /etc/os-release | sed -n 's/.*(\(.*\)).*/\1/p') no-subscription" > /etc/apt/sources.list.d/ceph-no-enterprise.list && apt-get update
 
 # Remove nag
-echo "DPkg::Post-Invoke { \"dpkg -V proxmox-widget-toolkit | grep -q '/proxmoxlib\.js$'; if [ \$? -eq 1 ]; then { echo 'Removing subscription nag from UI...'; sed -i '/data.status/{s/\!//;s/Active/NoMoreNagging/}' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js; }; fi\"; };" > /etc/apt/apt.conf.d/no-nag-script && apt --reinstall install proxmox-widget-toolkit
+echo "DPkg::Post-Invoke { \"if [ -s /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js ] && ! grep -q -F 'NoMoreNagging' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js; then echo 'Removing subscription nag from UI...'; sed -i '/data\.status/{s/\!//;s/active/NoMoreNagging/}' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js; fi\" };" >/etc/apt/apt.conf.d/no-nag-script && apt --reinstall install proxmox-widget-toolkit
 
